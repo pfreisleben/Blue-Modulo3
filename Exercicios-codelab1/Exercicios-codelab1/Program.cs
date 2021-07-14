db7bc06aa66b478985a75ae5e6eec18316866d11
pfreisleben <pedrofreislebeen@gmail.com>
2021-07-13 21:34:49 -0300

    ok

---
+++ Exercicios-codelab1/Exercicios-codelab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+//Exercicio 1 - Fatorial de um número
+Console.WriteLine("Informe um número para que o fatorial seja calculado: ");
+int numFatorial = Convert.ToInt32(Console.ReadLine());
+int multiplicador = numFatorial;
+int resultado = numFatorial;
+while(multiplicador > 1)
+{
+    resultado *= --multiplicador;
+}
+
+Console.WriteLine($"O fatorial de {numFatorial} é: {resultado}");
+
+
+//Exercicio 2 - Lista de números
+
+Console.WriteLine("Digite o tamanho da lista.");
+int listLength = Convert.ToInt32(Console.ReadLine());
+
+
+
+
