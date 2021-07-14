f3249954e5a4d293faec8f73f376f9091e9e07f1
pfreisleben <pedrofreislebeen@gmail.com>
2021-07-13 22:16:49 -0300

    ok

--- Exercicios-codelab1/Exercicios-codelab1/Program.cs
+++ Exercicios-codelab1/Exercicios-codelab1/Program.cs
@@ -15,8 +15,15 @@ Console.WriteLine($"O fatorial de {numFatorial} é: {resultado}");
 
 //Exercicio 2 - Lista de números
 
-Console.WriteLine("Digite o tamanho da lista.");
-int listLength = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Quantos números você deseja adicionar?");
+int tamanho = Convert.ToInt32(Console.ReadLine());
+int[] valores = new int[tamanho];
+
+for (int i = 0; i < tamanho; i++)
+{
+    Console.WriteLine($"Adicione o {i + 1}º valor");
+    valores[i] = Convert.ToInt32(Console.ReadLine());
+}
 
 
 
