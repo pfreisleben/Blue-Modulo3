bfd66f227ed5969a88be5d26def3905672658279
pfreisleben <pedrofreislebeen@gmail.com>
2021-07-15 10:43:49 -0300

    ok

--- JogoRPG/Heroi.cs
+++ JogoRPG/Heroi.cs
@@ -6,11 +6,43 @@ namespace JogoRPG
   public class Heroi
   {
 
-    public string Nome;
+    public string Nome
+    { get; set; }
     private int _experiencia = 0;
+
+    public int Experiencia
+    {
+      get => _experiencia;
+      set { _experiencia += value; }
+    }
     private int _nivel = 1;
+    public int Nivel
+    {
+      get => _nivel;
+      set { _nivel += value; }
+    }
+
     private int _vida = 10;
-    private int AtaqueBase;
+    public int Vida
+    {
+      get => _vida;
+      set { _vida += value; }
+    }
+    private int _ataqueBase;
+    public int ataqueBase
+    {
+      get => _ataqueBase;
+      set { _ataqueBase += value; }
+    }
+
+    public void mostrarHeroi()
+    {
+      Console.WriteLine($"Herói {_nome} criado com sucesso!");
+      Console.WriteLine($"Experiência: {_experiencia}.");
+
+
+    }
+
 
 
 
