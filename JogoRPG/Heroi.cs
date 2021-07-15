cf9f3cdd9d382837ac9017826650c1c95827dbfa
pfreisleben <pedrofreislebeen@gmail.com>
2021-07-15 09:30:31 -0300

    ok

---
+++ JogoRPG/Heroi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JogoRPG
+{
+  public class Heroi
+  {
+
+    public Heroi(string nome, int experiencia, int nivel, int vida)
+    {
+      
+
+    }
+
+    
+
+  }
+}
---
+++ JogoRPG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JogoRPG
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Jogo jogo = new Jogo();
+    }
+  }
+}
